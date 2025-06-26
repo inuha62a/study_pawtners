54027df50bdddebb3dddcc2a1d8659ffeade7760
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-26 13:53:02 +0900

    migrationファイル編集、migrate

--- db/migrate/20250626044335_devise_create_users.rb
+++ db/migrate/20250626044335_devise_create_users.rb
@@ -4,9 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+
+      # デモモード
+      t.boolean :is_demo, default: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
