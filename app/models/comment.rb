9b43b648846a9a356d3ea63f02e1eadb3b15d0f8
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-26 23:19:39 +0900

    comment.eb編集

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
-end
+    belongs_to :user
+    belongs_to :article
+    has_one_attached :image
+  
+    TAG_OPTIONS = {
+      official: "公式",
+      article: "記事URL",
+      ai_prompt: "AIプロンプト",
+      other: "その他"
+    }.freeze
+  end
+  
