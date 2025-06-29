7b57a74acfb4e67adb2acad6a728227637b34a19
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-29 21:22:21 +0900

    検証7-2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
+  # belongs_to :user
+  # has_many :comments, dependent: :destroy
 
   # enum :category, { error_log: 0, ai_prompt: 1 }
   # enum :status, { draft: 0, published: 1 }
 
-  validates :title, presence: true
-  has_one_attached :image
+  # validates :title, presence: true
+  # has_one_attached :image
 end
