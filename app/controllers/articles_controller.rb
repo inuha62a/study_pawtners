5180c33fc3b936f8e46250e3622bc1cec5320bba
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-06-29 17:02:28 +0900

    index検証②

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
     before_action :correct_user!, only: [ :edit, :update, :destroy ]
 
     def index
-      @user = current_user
+      @search_form = ArticleSearchForm.new(search_params)
       render plain: "Articles index is working!"
     end
 
