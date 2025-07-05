e220fbaf8d94cf788700ce79985d3be23dc1baee
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-07-05 18:23:25 +0900

    学習記録デフォルトで今日の日付になる

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -1,12 +1,18 @@
 class StudyRecord < ApplicationRecord
-    belongs_to :user
-    has_many :learning_studies, dependent: :destroy
-    has_many :learning_items, through: :learning_studies
-    has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :learning_studies, dependent: :destroy
+  has_many :learning_items, through: :learning_studies
+  has_many :comments, dependent: :destroy
 
-    def store_contents_names
-      self.contents_names = contents.map(&:name).join("\n")
-    end
+  def store_contents_names
+    self.contents_names = contents.map(&:name).join("\n")
+  end
 
-    validates :date, presence: true
+  validates :date, presence: true
+
+  private
+
+  def set_default_date
+    self.date ||= Date.current
+  end
 end
